babf2369bad12141419aecc4dbdd43734b7981a7
pisu86 <mateuszpiotrpis@gmail.com>
2015-03-10 21:16:38 -0500

    Initial Commit

---
+++ Photographer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photographer
+//
+//  Created by Matt on 3/10/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photographer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photographer
+//
+//  Created by Matt on 3/10/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
